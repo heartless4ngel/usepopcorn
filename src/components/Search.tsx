4620f7c2707c5cac1abb7a6c7d81d0c89aa2c4cd
heartless4ngel <mattialancellotta@hotmail.com>
2025-10-10 21:23:07 +0200

    feat: add focus search bar on enter click

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useRef } from "react";
+
 type SearchProps = {
   query: string;
   setQuery: (query: string) => void;
 };
 
 export function Search({ query, setQuery }: SearchProps) {
+  const inputEl = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    function callback(e: KeyboardEvent) {
+      if (e.code === "Enter") {
+        if (document.activeElement === inputEl.current) return;
+        inputEl.current?.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
+      autoFocus
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
       onChange={e => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
